6ebb6be60b8bfeca82c91f4fcacf708712cfb3f2
Dima Skibin <sixtynice77@gmail.com>
2019-12-14 15:44:37 +0300

    include component: Panel, Weather,NoteApp

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './main.scss';
 import Panel from './components/Panel/Panel.jsx';
 import Weather from './components/Weather/Weather.jsx'
+import NoteApp from './components/NoteApp/NoteApp';
 
 export default function App() {
 
     return (
         <div className="App">
-            <Panel />
             <Weather />
+
+            <NoteApp />
+            <Panel />
         </div>
-    );  
+    );
 }
