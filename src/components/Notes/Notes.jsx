9e508e6ea57b52cb0bd976a2f05be7337e0e12a1
Dima Skibin <sixtynice77@gmail.com>
2019-12-14 15:42:42 +0300

    Begin Notes component

---
+++ src/components/Notes/Notes.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Notes extends Component {
+
+
+    render() {
+        return (
+            <div className="Notes">
+                <textarea name="" id="" cols="30" rows="10">
+                    
+                </textarea>
+            </div>
+        )
+    }
+}
